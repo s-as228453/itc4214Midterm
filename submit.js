2b69b73a2a7a2b9c7a31f881dd0c779b7fc62bcc
s-as228453 <90832314+s-as228453@users.noreply.github.com>
2021-11-01 22:01:14 +0200

    fixed bug in button

--- submit.js
+++ submit.js
@@ -1,8 +1,15 @@
 function AddData() {
-    var rows = "";
-    var surname = document.getElementById("surname").value;
-    var grade = document.getElementById("grade").value;
-    rows += "<tr><td>" + surname + "</td><td>" + grade + "</td></tr> ";
-    $(rows).appendTo("#table tbody");
-    return false;
-  }
+  var rows = "";
+  var name = document.getElementById("surname").value;
+  var num = document.getElementById("grade").value;
+  rows += "<tr><td>" + name + "</td><td>" + num + "</td><td><a href='javascript:void(0);' class='remCF1 btn btn-small btn-danger'>Delete</a></td></tr>";
+  $(rows).appendTo("#table tbody");
+
+  $(document).on('click','.remCF1',function(){
+      $(this).parent().parent().remove();
+      $('#myTable tbody tr').each(function(i){            
+        $($(this).find('td')[0]).html(i+1);          
+      });
+  });
+  return false;
+}
